b9f3a5f36045f81b8cd0878a668dadaae3dcae03
Dominik <domino1661@gmail.com>
2022-11-13 11:16:19 +0100

    13.11.2022

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Message } from '../shared/models/message';
-import { Observable, combineLatest, map } from 'rxjs';
+import { Observable, combineLatest, map, from, switchMap, tap } from 'rxjs';
 import { UserService } from '../services/user.service';
 import { User } from '../shared/models/user';
 import { AuthUser } from '../Auth/authuser.model';
@@ -15,20 +15,23 @@ import { Chat } from '../shared/models/chat';
   styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent implements OnInit {
+  @ViewChild('chatEnd')
+  chatEnd!: ElementRef;
+
   username: string = '';
-  message: string = '';
   messages!: Observable<Message[]>;
   searchControl = new FormControl('');
   users: Observable<User[]> = this.userService.getallUsers();
-  currentUserEmail: string | undefined = '';
-  myChats:Observable<Chat[]> = this.chatService.myChats;
-  chatDisplayName:string | undefined = ''
+  currentUser!: User | null;
+  myChats: Observable<Chat[]> = this.chatService.myChats;
+  chatDisplayName: string | undefined = '';
   chatListControl = new FormControl('');
+  messageControl = new FormControl('');
+  chatAllMessages: Message[] = []
   selectedChat = combineLatest([
     this.chatListControl.valueChanges,
     this.myChats,
   ]).pipe(map(([value, chats]) => chats.find((c) => c.id === value![0])));
-
 
   constructor(
     private userService: UserService,
@@ -36,17 +39,39 @@ export class ChatComponent implements OnInit {
     private chatService: ChatService
   ) {}
 
-
   ngOnInit(): void {
     this.userService.CurrentAuthUSer.subscribe(
-      (user) => (this.currentUserEmail = user?.email)
+      (user) => (this.currentUser = user)
     );
-      this.chatListControl.valueChanges.subscribe(data=> console.log(data))
-    }
+
+    this.chatListControl.valueChanges.pipe(
+      map((value) => value![0]),
+      switchMap((chatID) => this.chatService.getChatMessages(chatID)),
+      tap(() => {
+        this.scrollToBottom();
+      })
+    ).subscribe(messsage => this.chatAllMessages = messsage);
+  }
 
   createChat(user: User) {
     this.chatService.createChat(user).subscribe();
   }
 
-  onSubmit() {}
+  onSubmit() {
+    const message = this.messageControl.value;
+    const selectedChatID: any = this.chatListControl.value;
+
+    if (message && selectedChatID[0]) {
+      this.chatService.addChatMessage(selectedChatID[0], message).subscribe();
+        this.messageControl.setValue('');
+    }
+  }
+
+  scrollToBottom() {
+    setTimeout(() => {
+      if (this.chatEnd) {
+        this.chatEnd.nativeElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 10);
+  }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import {
+  doc,
   addDoc,
+  updateDoc,
   collectionData,
   Firestore,
   query,
   where,
+  Timestamp,
+  collection,
+  orderBy,
 } from '@angular/fire/firestore';
-import { collection } from '@firebase/firestore';
 import { Observable } from 'rxjs';
-import { concatMap, take, map, switchMap } from 'rxjs/operators';
+import { concatMap, take, map } from 'rxjs/operators';
 import { Chat } from '../shared/models/chat';
+import { Message } from '../shared/models/message';
 import { User } from '../shared/models/user';
 import { UserService } from './user.service';
 
@@ -51,22 +56,47 @@ export class ChatService {
           where('userIDs', 'array-contains', user?.email)
         );
         return collectionData(myQuery, { idField: 'id' }).pipe(
-          map((chats:any) => this.addChatNameAndPic(user?.uid ?? '', chats))
+          map((chats: any) => this.addChatNameAndPic(user?.uid ?? '', chats))
         ) as Observable<Chat[]>;
       })
     );
   }
 
   addChatNameAndPic(currentUserEmail: string, chats: Chat[]): Chat[] {
-    chats.forEach((chat:Chat) => {
+    chats.forEach((chat: Chat) => {
       // if (chat.userIds) {
-        // const otherIndex = chat.userIds.indexOf(currentUserEmail ?? '') === 0 ? 1 : 0;
-        const { displayName, photoUrl } = chat.users[1];
-        chat.chatName = displayName;
-        chat.chatImg = photoUrl;
+      // const otherIndex = chat.userIds.indexOf(currentUserEmail ?? '') === 0 ? 1 : 0;
+      const { displayName, photoUrl } = chat.users[1];
+      chat.chatName = displayName;
+      chat.chatImg = photoUrl;
       // }
     });
 
     return chats;
+  }
+
+  addChatMessage(chatId: string, message: string): Observable<any> {
+    const ref = collection(this.firestore, 'chats', chatId, 'messages');
+    const chatRef = doc(this.firestore, 'chats', chatId);
+    const today = Timestamp.fromDate(new Date());
+    return this.userService.CurrentAuthUSer.pipe(
+      take(1),
+      concatMap((user) =>
+        addDoc(ref, {
+          message: message,
+          author: user?.uid,
+          sentDate: today,
+        })
+      ),
+      concatMap(() =>
+        updateDoc(chatRef, { lastMessage: message, lastMessageDate: today })
+      )
+    );
+  }
+
+  getChatMessages(chatId: string): Observable<Message[]> {
+    const ref = collection(this.firestore, 'chats', chatId, 'messages');
+    const queryAll = query(ref, orderBy('sentDate', 'asc'));
+    return collectionData(queryAll) as Observable<Message[]>;
   }
 }
